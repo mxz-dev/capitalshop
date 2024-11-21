2e85ac9ffca4af853226d91ac93910558ed8c632
mxz-dev <zabetzadehmohammad@gmail.com>
2024-11-21 19:58:06 +0330

    removed pin option from mag

--- mag/urls.py
+++ mag/urls.py
@@ -5,8 +5,6 @@ from . import views
 app_name = "mag"
 urlpatterns = [
     path("", views.blog_view , name="blog"),
-
     path("category/<str:cat>", views.blog_view , name="category"),
-
     path("post/<slug:slug>", views.post_view , name="post"),
 ]
--- mag/views.py
+++ mag/views.py
@@ -1,29 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from mag.models import Post, PostCategories, PostTags
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator
 from django.utils.timezone import now
 from django.db.models import Q
 # Create your views here.
 def blog_view(request, cat=None):
-    posts = Post.objects.filter(is_published=True, publish_at__lt=now()).order_by("-created_at").exclude(pin=True)
-    pinned_post = Post.objects.filter(is_published=True, publish_at__lt=now(),pin=True).order_by("pin","-created_at").first()
+    posts = Post.objects.filter(is_published=True, publish_at__lt=now()).order_by("-created_at")
     if c := cat:
-        posts = Post.objects.filter(is_published=True, publish_at__lt=now(),category__category_name=c).order_by("-created_at")
-        pinned_post = None
+        posts = posts.filter(category__category_name=c)
     if search_query := request.GET.get('search'):
-        posts = Post.objects.filter(Q(subject__contains=search_query) | Q(content__contains=search_query),is_published=True, publish_at__lt=now() ).order_by("-created_at")
-        pinned_post = None
-
-    paginator = Paginator(posts, 4)
+        posts = Post.objects.filter(Q(subject__contains=search_query) | Q(content__contains=search_query), is_published=True, publish_at__lt=now()).order_by("-created_at")
+   
+    # pagination the searched and categorized posts
+    paginator = Paginator(posts, 6)
     page_num = request.GET.get('page')
     try:
         page_obj = paginator.page(page_num)
-    except EmptyPage:
-        page_obj = paginator.page(1)
-    except PageNotAnInteger:
-        page_obj = paginator.page(1)
-    
-    return render(request, 'blog/blog.html', {"page_obj":page_obj, "pin_post":pinned_post})
+    except:
+        page_obj = paginator.get_page(1)
+    return render(request, 'blog/blog.html', {"page_obj":page_obj})
 
 def post_view(request, slug):
     post = get_object_or_404(Post, is_published=True, publish_at__lt=now(), slug=slug)
