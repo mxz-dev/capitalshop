abb8237faf1e5c4f9202f3e9b863bbe9c9865566
mxz-dev <zabetzadehmohammad@gmail.com>
2024-11-25 16:46:35 +0330

    remove print()

--- accounts/signals.py
+++ accounts/signals.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 
 @receiver(post_save , sender=User)
 def user_to_inactive(sender, instance, created, update_fields, **kwargs):
-    print(created, instance)
     if created:
         User.objects.filter(username=instance).update(is_active=False)
         
