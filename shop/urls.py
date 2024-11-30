71cb351b00a9836e587e2a6b82af0cb8b18bc036
mxz-dev <zabetzadehmohammad@gmail.com>
2024-11-30 17:28:20 +0330

    staging for cart...

--- shop/urls.py
+++ shop/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path("shop", views.shop_view , name="shop"),
     path("product/<slug:slug>", views.shopitem_view, name="shopitem"),
     path("about", views.about_view, name="about"),
-    path("contact", views.contact_view, name="contact")
+    path("contact", views.contact_view, name="contact"),
+    path("cart", views.cart_view, name="cart"),
+    path("cart/add/<str:product>/<int:quantity>", views.add_to_cart_view, name="add_to_cart"),
+    path("cart/remove/<str:product>", views.remove_from_cart_view, name="remove_from_cart"),
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render , get_object_or_404
-from shop.models import Products, ProductCategories
+from django.shortcuts import render , get_object_or_404, HttpResponse
+from shop.models import Products, ProductCategories, Cart, CartItem
 from shop.forms import ContactForm
 from mag.models import Post
+from django.contrib.auth.models import User
 from django.db.models import Q
 from django.utils.timezone import now
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+
 def home_view(request):
     products = Products.objects.filter(in_stock=True).order_by('-created_at')[:4]
     posts = Post.objects.filter(is_published=True, publish_at__lt=now()).order_by('-created_at')[:3]
@@ -28,3 +31,29 @@ def contact_view(request):
             form.save()
             messages.add_message(request, messages.SUCCESS, 'we are responds you very soon.')
     return render(request, 'contact.html', {"form":form})
+
+@login_required
+def cart_view(request):
+    pass
+@login_required
+def add_to_cart_view(request, product, quantity):
+    user = User.objects.get(username=request.user)
+    try:
+        cart = Cart.objects.get(created_by=user.id)
+        print(cart.id)
+
+        product = Products.objects.get(id=product, in_stock=True)
+    # if not cart:
+    except:
+        cart = Cart.objects.create(created_by=user, status="active")
+        print(cart)
+    try:
+        cart_item = CartItem.objects.create(cart=cart, product=product, price=product.price, quantity=quantity)
+        print(cart_item)
+    
+    except:
+        print(cart_item)
+    return HttpResponse("done")
+@login_required
+def remove_from_cart_view(request):
+    pass
